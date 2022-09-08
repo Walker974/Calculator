fe99968cb26df17ff01c7e76177a2da6d8cec2d4
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-08 20:01:14 +0100

    Update

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    printf("First Number :");
+    scanf("%f", &a);
+
+    printf("Second Number :");
+    scanf("%f", &b);
+
+    printf("Sum : %f", a + b);
+    return 0;
+}
