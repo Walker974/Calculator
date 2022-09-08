fdb7b407ec119012c58e069c840fbf2b1efb5e46
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-08 19:56:49 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("First Number : ");
+    scanf("%d", &a);
+
+    printf("Second Number : ");
+    scanf("%d", &b);
+
+    printf("Sum : %d", a + b);
+    return 0;
+}
