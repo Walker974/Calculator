5fb9081cf33b55b06addcb67ddf0dd539c3c6fa3
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-16 22:21:26 +0100

    Update

--- src/main.c
+++ src/main.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 
+double addition(double a, double b) {
+    return (a + b);
+}
+double substraction(double a, double b) {
+    return (a - b);
+}
+double multiplication(double a, double b) {
+    return (a * b);
+}
+double division(double a, double b) {
+    return (a / b);
+}
 int main() {
-    float a, b;
-    printf("First Number :");
-    scanf("%f", &a);
+    double first_Number, second_Number, result;
+    char operator;
 
-    printf("Second Number :");
-    scanf("%f", &b);
+    printf("Enter a number:");
+    scanf("%lf", &first_Number);
+    printf("Enter operator:");
+    scanf(" %c", &operator);
+    printf("Enter a number:");
+    scanf("%lf", &second_Number);
 
-    printf("Sum : %f", a + b);
+    if (operator == '+') {
+        result = addition(first_Number, second_Number);
+        printf("Result: %f", result);
+    } else if (operator == '-') {
+        result = substraction(first_Number, second_Number);
+        printf("Result: %f", result);
+    } else if (operator == '*') {
+        result = multiplication(first_Number, second_Number);
+        printf("Result: %f", result);
+    } else if (operator == '/') {
+        result = division(first_Number, second_Number);
+        printf("Result: %f", result);
+    } else {
+        printf("Error! operator is not correct");
+    }
     return 0;
 }
